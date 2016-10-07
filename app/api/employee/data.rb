d664a9359b270642f738429cea831022089dfd99
Lovinder Pnag <lspnag@gmail.com>
2016-10-07 10:35:37 -0700

    update an employee address

--- app/api/employee/data.rb
+++ app/api/employee/data.rb
@@ -32,7 +32,16 @@ module Employee
         EmpData.find(params[:id]).destroy!
       end
 
-      
+      desc "update an employee address"
+      params do 
+        requires :id, type: String
+        requires :address, type: String
+      end
+      put ':id' do
+        EmpData.find(params[:id]).update({
+          address:params[:address]
+        })
+      end
 
     end
 
